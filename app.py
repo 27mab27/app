065340e56e7a3285f6eb4899efdd6f3a41999abe
27mab27 <157074512+27mab27@users.noreply.github.com>
2025-04-29 19:37:40 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template_string
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = '/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        filepath = os.path.join(UPLOAD_FOLDER, f.filename)
+        f.save(filepath)
+        return f'File uploaded to {filepath}'
+    return '''
+    <!doctype html>
+    <title>Upload File</title>
+    <h1>Upload a File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
